2a4058b4436b3851c85d63ee02c8417efd3a5893
Anna Yudina <annayudi@gmail.com>
2021-12-06 20:13:57 +0300

    Day 2: cleanup

--- src/main/scala/Day02.scala
+++ src/main/scala/Day02.scala
@@ -2,26 +2,29 @@ import Util.readFile
 
 import scala.io.Source
 
-def applyCommand(coordinates: (Int, Int), command: String): (Int, Int) = {
+def applyCommand(coordinates: (Int, Int), s: String): (Int, Int) = {
   val (x, y) = coordinates
-  val split = command.split(' ')
-  val v = split(1).toInt
-  split.head match {
+  val (command, v) = parseCommand(s)
+  command match {
     case "forward" => (x + v, y)
     case "down" => (x, y + v)
     case "up" => (x, y - v)
   }
 }
 
-def applyCommandWithAim(position: (Int, Int, Int), command: String): (Int, Int, Int) = {
+def applyCommandWithAim(position: (Int, Int, Int), s: String): (Int, Int, Int) = {
   val (x, y, aim) = position
-  val split = command.split(' ')
-  val v = split(1).toInt
-  split.head match {
+  val (command, v) = parseCommand(s)
+  command match {
     case "forward" => (x + v, y + (aim * v), aim)
     case "down" => (x, y, aim + v)
     case "up" => (x, y, aim - v)
   }
+}
+
+def parseCommand(s: String): (String, Int) = {
+  val split = s.split(' ')
+  (split(0), split(1).toInt)
 }
 
 @main def day02(): Unit = {
