22f6b182ea0ebdcc43b136609eab6b68d1ccd715
Anna Yudina <annayudi@gmail.com>
2021-12-18 15:05:18 +0100

    Day 13

---
+++ src/main/scala/Day13.scala
@@ -0,0 +1,41 @@
+import Util.{Coordinates, readFile}
+
+import scala.annotation.tailrec
+
+@main def day13(): Unit = {
+  val input = readFile("resources/day13")
+
+  val coordinates: List[Coordinates] = input.takeWhile(!_.isBlank).map { x =>
+    val split = x.split(",").map(_.toInt)
+    split.head -> split.last
+  }
+
+  val folds = input.drop(coordinates.length + 1).map { x =>
+    val split = x.stripPrefix("fold along ").split("=")
+    val axis = split.head
+    val coordinate = split.last.toInt
+    axis -> coordinate
+  }
+
+  def fold(input: List[Coordinates], fold: (String, Int)): List[Coordinates] = input.map { case (x, y) =>
+    fold._1 match {
+      case "x" =>
+        if (x > fold._2) (2 * fold._2 - x) -> y
+        else x -> y
+      case "y" =>
+        if (y > fold._2) x -> (2 * fold._2 - y)
+        else x -> y
+    }
+  }.distinct
+
+  println(fold(coordinates, folds.head).size)
+
+  val xs = folds.foldLeft(coordinates)(fold)
+
+  println(
+    (0 to xs.map(_._2).max).map { y =>
+      (0 to xs.map(_._1).max).map { x =>
+        if (xs.contains(x -> y)) '#' else '.'
+      }.mkString("")
+    }.mkString("\n"))
+}
